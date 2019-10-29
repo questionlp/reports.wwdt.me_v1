1c9a14585bdfdaf4b001a727084d560247d22ccd
Linh Pham <question+github@closedsrc.org>
2019-10-28 22:11:02 -0700

    Start work on panel gender mix report

---
+++ reports/panelist/gender_mix.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018-2019 Linh Pham
+# reports.wwdt.me is relased under the terms of the Apache License 2.0
+"""WWDTM Panel Gender Mix Report Functions"""
+
+from collections import OrderedDict
+from typing import List, Dict
+import mysql.connector
+from mysql.connector import DatabaseError, ProgrammingError
+
+#region Retrieval Functions
+
+
+
+#endregion
