1fab9735777683dc22c17932a627684965f4b29e
Linh Pham <dev@wwdt.me>
2020-01-21 09:59:07 -0800

    Update copyright year

--- reports/__init__.py
+++ reports/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2018-2019 Linh Pham
+# Copyright (c) 2018-2020 Linh Pham
 # reports.wwdt.me is relased under the terms of the Apache License 2.0
 """Explicitly listing all reporting modules"""
 
