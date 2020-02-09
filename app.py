56c5133e9d559c41f2d0ec03dd26f510e02e561a
Linh Pham <question+github@closedsrc.org>
2020-02-08 19:28:23 -0800

    Clean up requirements.txt and bump versions

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ from reports.show import (all_women_panel, guest_hosts, guest_scorekeeper,
                           lightning_round, show_details)
 
 #region Global Constants
-APP_VERSION = "1.4.5.2"
+APP_VERSION = "1.4.6"
 RANK_MAP = {
     "1": "First",
     "1t": "First Tied",
