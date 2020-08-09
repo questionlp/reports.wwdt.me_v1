bb9a63cf041708368ecc0602ea7dd5143c88b4ba
Linh Pham <dev@wwdt.me>
2020-08-08 18:12:09 -0700

    Bump version

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ from reports.show import (all_women_panel, guest_hosts, guest_scorekeeper,
                           high_scoring, lightning_round, show_details)
 
 #region Global Constants
-APP_VERSION = "1.8.1"
+APP_VERSION = "1.9.0"
 RANK_MAP = {
     "1": "First",
     "1t": "First Tied",
