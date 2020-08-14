79a71b98ea61e189de161e70327fc93a8bd77169
Linh Pham <dev@wwdt.me>
2020-08-14 11:50:52 -0700

    Adding Guest Most Appearances Report

--- reports/utility.py
+++ reports/utility.py
@@ -3,7 +3,10 @@
 # reports.wwdt.me is relased under the terms of the Apache License 2.0
 """Utility functions used by the Reports Site"""
 
+from functools import cmp_to_key
 from datetime import datetime
+from operator import itemgetter as i
+from typing import Dict, List
 from dateutil import parser
 import pytz
 
@@ -44,5 +47,34 @@ def time_zone_parser(time_zone: str) -> pytz.timezone:
         time_zone_string = time_zone_object.zone
 
     return time_zone_object, time_zone_string
+
+#endregion
+
+#region Sorting Functions
+def cmp(object_a, object_b):
+    """Replacement for built-in function cmp that was removed in Python 3
+
+    Compare the two objects a and b and return an integer according to
+    the outcome. The return value is negative if a < b, zero if a == b
+    and strictly positive if a > b.
+
+    https://portingguide.readthedocs.io/en/latest/comparisons.html#the-cmp-function
+    """
+
+    return (object_a > object_b) - (object_a < object_b)
+
+def multi_key_sort(items: List[Dict], columns: List) -> List[Dict]:
+    """Sorts a list of dictionaries based on a list of one or more keys"""
+    comparers = [
+        ((i(col[1:].strip()), -1) if col.startswith('-') else (i(col.strip()), 1))
+        for col in columns
+    ]
+    def comparer(left, right):
+        comparer_iter = (
+            cmp(fn(left), fn(right)) * mult
+            for fn, mult in comparers
+        )
+        return next((result for result in comparer_iter if result), 0)
+    return sorted(items, key=cmp_to_key(comparer))
 
 #endregion
