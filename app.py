37c0fb78042d3bd0d275d2d79fa7be54f9be9d3d
Linh Pham <dev@wwdt.me>
2022-03-28 14:13:12 -0700

    Adding appearance counts columns to First and Most Recent Appearances report

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ from reports.show import (all_women_panel,
                           show_details)
 
 #region Global Constants
-APP_VERSION = "1.17.0"
+APP_VERSION = "1.18.0"
 RANK_MAP = {
     "1": "First",
     "1t": "First Tied",
